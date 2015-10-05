7066044bbb3e372e00a6726f2ca602ffb514e81c
ivankp <ivan.pogrebnyak@gmail.com>
2015-10-05 14:55:27 -0500

    fcns start with _

--- senum.hh
+++ senum.hh
@@ -26,21 +26,21 @@
 #define senum(enum_name, values) \
   struct enum_name { \
     enum type { BOOST_PP_SEQ_ENUM(values) }; \
-    enum { nelem = BOOST_PP_SEQ_SIZE(values) }; \
-    static type en(const std::string& str) { \
+    enum { _nelem = BOOST_PP_SEQ_SIZE(values) }; \
+    static type _enum(const std::string& str) { \
       BOOST_PP_SEQ_FOR_EACH( SENUM_STR_CASE, nil, values ) \
       throw std::runtime_error( \
         "invalid value \""+str+"\" specified for stringized enum " \
         BOOST_PP_STRINGIZE(enum_name) \
       ); \
     } \
-    static const char* str(type en) { \
+    static const char* _str(type en) { \
       switch (en) { \
         BOOST_PP_SEQ_FOR_EACH( SENUM_EN_CASE, nil, values ) \
       } \
       return 0; \
     } \
-    static const char* str_all { \
+    static const char* _str_all() { \
       return BOOST_PP_SEQ_FOR_EACH( SENUM_CS, nil, values ); \
     } \
   }; \
@@ -48,13 +48,14 @@
 std::istream& operator>> (std::istream& is, enum_name::type& x) { \
   std::string str; \
   is >> str; \
-  x = enum_name::get_enum(str); \
+  x = enum_name::_enum(str); \
   return is; \
 } \
 \
 std::ostream& operator<< (std::ostream& os, const enum_name::type& x) { \
-  os << enum_name::get_str(x); \
+  os << enum_name::_str(x); \
   return os; \
 }
   
 #endif
+
--- test.cc
+++ test.cc
@@ -5,13 +5,13 @@ senum(numbers,(one)(two)(three))
 #ifndef senum_pp_test
 int main(int argc, char **argv)
 {
-  std::cout << numbers::get_str(numbers::one)   << ' ';
-  std::cout << numbers::get_str(numbers::two)   << ' ';
-  std::cout << numbers::get_str(numbers::three) << std::endl;
+  std::cout << numbers::_str(numbers::one)   << ' ';
+  std::cout << numbers::_str(numbers::two)   << ' ';
+  std::cout << numbers::_str(numbers::three) << std::endl;
 
-  std::cout << numbers::get_str(numbers::get_enum("one"))   << ' ';
-  std::cout << numbers::get_str(numbers::get_enum("two"))   << ' ';
-  std::cout << numbers::get_str(numbers::get_enum("three")) << std::endl;
+  std::cout << numbers::_str(numbers::_enum("one"))   << ' ';
+  std::cout << numbers::_str(numbers::_enum("two"))   << ' ';
+  std::cout << numbers::_str(numbers::_enum("three")) << std::endl;
 
   std::cout << numbers::one   << ' ';
   std::cout << numbers::two   << ' ';
